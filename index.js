e104bf24e82ff7672e6e5f4a8464968401b33fe6
Eamonn O'Brien-Strain <eobrain@gmail.com>
2024-06-16 20:33:44 -0700

    initial

---
+++ index.js
@@ -0,0 +1,40 @@
+import { toot } from './mastodon.js'
+import { optimize, currentPlace, relTime, BODY_TEMP, humanEffect, tile } from 'wetbulb'
+
+const api = (lat, lon) => `https://weather-424404.uc.r.appspot.com/?lat=${lat}&lon=${lon}`
+
+let count = 0
+const { worstPlace, worstResult } = await optimize(api,
+  data => {
+    console.log(++count, currentPlace())
+  })
+
+console.log(worstPlace, worstResult)
+
+const when = relTime(worstResult.date)
+const wetbulb = Math.round(worstResult.wetbulb)
+const sweatability = Math.round(BODY_TEMP - worstResult.wetbulb)
+const effect = humanEffect(worstResult.wetbulb)
+const humidity = Math.round(worstResult.humidity)
+const temp = Math.round(worstResult.temp)
+const feelsLike = Math.round(worstResult.feelsLike)
+
+toot(`
+${when} in ${worstResult.name}, ${worstResult.country}
+the wet-bulb temperature will be
+
+      ${wetbulb}°C
+
+This will be a margin of ${sweatability} degrees below body temperature
+which will ${effect}
+
+The humidity will be ${humidity}%
+
+The actual temperature will be ${temp}°C
+
+It will feel like ${feelsLike}°C
+
+There will will be ${worstResult.weather}
+
+${tile(worstPlace)}
+`)
---
+++ run.sh
@@ -0,0 +1,3 @@
+cd `dirname $0`
+. ./secrets.env
+node index.js
