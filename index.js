f2e57beb07e9fc25de6c0a061436c5d11709816f
Eamonn O'Brien-Strain <eobrain@gmail.com>
2024-06-17 22:26:10 -0700

    Add geocoding

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { toot } from './mastodon.js'
 import { optimize, currentPlace, relTime, BODY_TEMP, humanEffect } from 'wetbulb'
+import { geocode } from './geocode.js'
 
 const api = (lat, lon) => `https://weather-424404.uc.r.appspot.com/?lat=${lat}&lon=${lon}`
 
@@ -22,11 +23,12 @@ const temp = Math.round(worstResult.temp)
 const feelsLike = Math.round(worstResult.feelsLike)
 const { lat, lon } = worstPlace
 const mapUrl = `https://maps.google.com/?ll=${lat},${lon}&q=${lat},${lon}&z=8`
+const placeName = await geocode(lat, lon)
 
 // const searchUrl = `https://www.google.com/search?q=%22${encodeURIComponent(worstResult.name)}%22+${encodeURIComponent(worstResult.country)}+excessive+heat`
 
 toot(`
-${when} in ${worstResult.name}, #${country} there will be a #Heatwave with a wet-bulb temperature of
+${when} in ${worstResult.name}, #${country} (${placeName}) there will be a #Heatwave with a wet-bulb temperature of
 
       ${wetbulb}°C
 
