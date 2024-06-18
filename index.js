8a03c540166bba831f7d12e7c6eac4b3afb44bcf
Eamonn O'Brien-Strain <eobrain@gmail.com>
2024-06-17 20:48:18 -0700

    Fix country hashtagging

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { toot } from './mastodon.js'
-import { optimize, currentPlace, relTime, BODY_TEMP, humanEffect, tile } from 'wetbulb'
+import { optimize, currentPlace, relTime, BODY_TEMP, humanEffect } from 'wetbulb'
 
 const api = (lat, lon) => `https://weather-424404.uc.r.appspot.com/?lat=${lat}&lon=${lon}`
 
@@ -11,6 +11,8 @@ const { worstPlace, worstResult } = await optimize(api,
 
 console.log(worstPlace, worstResult)
 
+const capitalize = s => s[0].toUpperCase() + s.slice(1).toLowerCase()
+const country = worstResult.country.split(/[\s-]+/).map(capitalize).join('')
 const when = relTime(worstResult.date)
 const wetbulb = Math.round(worstResult.wetbulb)
 const sweatability = Math.round(BODY_TEMP - worstResult.wetbulb)
@@ -18,11 +20,13 @@ const effect = humanEffect(worstResult.wetbulb)
 const humidity = Math.round(worstResult.humidity)
 const temp = Math.round(worstResult.temp)
 const feelsLike = Math.round(worstResult.feelsLike)
+const { lat, lon } = worstPlace
+const mapUrl = `https://maps.google.com/?ll=${lat},${lon}&q=${lat},${lon}&z=8`
 
 // const searchUrl = `https://www.google.com/search?q=%22${encodeURIComponent(worstResult.name)}%22+${encodeURIComponent(worstResult.country)}+excessive+heat`
 
 toot(`
-${when} in ${worstResult.name}, #${worstResult.country} there will be a #Heatwave with a wet-bulb temperature of
+${when} in ${worstResult.name}, #${country} there will be a #Heatwave with a wet-bulb temperature of
 
       ${wetbulb}°C
 
@@ -36,7 +40,7 @@ The humidity will be ${humidity}%
 
 There will will be ${worstResult.weather}
 
-${tile(worstPlace)}
+${mapUrl}
 
-#Heatwave${worstResult.country}
+#Heatwave${country}
 `)
